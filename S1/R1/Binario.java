e341acbff2a1b41395aa255044ddedc462f6100a
Alma <arellano_alma@outlook.com>
2023-03-08 16:58:49 -0600

    Add files via upload

---
+++ S1/R1/Binario.java
@@ -0,0 +1,9 @@
+public class Binario {
+    public static void main(String[] args) {
+        int = 93;
+
+        String binary = Integer.toBinaryString(number);
+
+        System.out.println("El número entero" \" + number + \" en binario es: \" + binary);
+    }
+}

09a94780b436274a63b43cb0bb8952471e1a3973
Alma <arellano_alma@outlook.com>
2023-03-08 17:00:08 -0600

    Delete S1 directory

--- S1/R1/Binario.java
+++
@@ -1,9 +0,0 @@
-public class Binario {
-    public static void main(String[] args) {
-        int = 93;
-
-        String binary = Integer.toBinaryString(number);
-
-        System.out.println("El número entero" \" + number + \" en binario es: \" + binary);
-    }
-}
